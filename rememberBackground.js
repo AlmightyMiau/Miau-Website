a38cc0514f5af1b9fce0fb9c49e5221395ff6171
AlmightyMiau <almightymeow612@gmail.com>
2025-07-18 00:29:06 +0900

    apply onclick function to all images

--- rememberBackground.js
+++ rememberBackground.js
@@ -4,9 +4,15 @@ function remember(img) {
 function recall() {
     if (localStorage.getItem("background")) {
         console.log(localStorage.getItem("background"));
-        document.body.style.backgroundImage = 'url("https://almightymiau.gay/' + localStorage.getItem("background") + '")';
+        document.body.style.backgroundImage = "url('" + localStorage.getItem("background") + "')";
+    }
+}
+function setup() {
+    for (let image of document.getElementsByTagName("img")) {
+        image.onclick = function() {sillyBackground(image.src)};
     }
 }
 window.onload = () => {
     recall();
+    setup();
 }
